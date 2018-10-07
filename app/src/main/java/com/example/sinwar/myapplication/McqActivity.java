e0b9837616c77aa66c36f7379d608b8d0cb08d46
sinwar <premsinwar4@gmail.com>
2018-10-08 01:50:57 +0530

    develop listview current affairs

--- app/src/main/java/com/example/sinwar/myapplication/McqActivity.java
+++ app/src/main/java/com/example/sinwar/myapplication/McqActivity.java
@@ -4,6 +4,8 @@ import android.os.Bundle;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
 
 public class McqActivity extends AppCompatActivity {
 
@@ -13,8 +15,35 @@ public class McqActivity extends AppCompatActivity {
         setContentView(R.layout.mcq_design);
 
         // set menu bar here
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        ActionBar actionbar = getSupportActionBar();
+        actionbar.setDisplayHomeAsUpEnabled(true);
+        actionbar.setHomeAsUpIndicator(R.drawable.menu_bar);
+        actionbar.setDisplayShowTitleEnabled(false);
+    }
 
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
 
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
 
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_item_one) {
+
+            // Do something
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 }
